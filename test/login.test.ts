446d5465caaa11fca96f633c40e534bed725f95c
Koushik <koushik350@gmail.com>
2021-04-02 20:34:46 +0530

    📦 NEW: EP 5 - Videos

--- test/login.test.ts
+++ test/login.test.ts
@@ -5,7 +5,15 @@ describe('Launch Browser', () => {
         const browser = await chromium.launch({
             headless: false
         });
-        const context = await browser.newContext();
+        const context = await browser.newContext({
+            recordVideo: {
+                dir: "./videos/",
+                size: {
+                    width: 800,
+                    height: 600
+                }
+            }
+        });
 
         // Open new page
         const page = await context.newPage();
